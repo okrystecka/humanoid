a00c64cd674219d75c839424d0939acb3deb1ad1
Paweł Żakieta <kanar9123m2@wp.pl>
2018-06-25 10:46:03 +0200

    fuck zepsulem

---
+++ dynamixel_lib/Src/AX-12.h
@@ -0,0 +1,137 @@
+
+//biblioteka moze dziala, moze dziala tez dla innych dynamixeli
+
+#include "stm32f4xx_hal.h"
+
+
+//na uk³adzie max3485 jest pin okreslajacy w ktora strone odbywa sie transmisja
+//chyba w naszych ukladach stan wysoki to transmisja stm -> dynamixel
+//trzeba podpiac do pinu na stm, np 9a
+
+#define DIRECTION_PIN GPIO_PIN_9
+#define DIRECTION_PORT GPIOA
+#define SEND GPIO_PIN_SET
+#define RECEIVE GPIO_PIN_RESET
+
+uint8_t checkSum(uint8_t *data, int size){
+	uint8_t sum = 0x00;
+	for (int i = 2; i<size; i++){
+		sum += data[i];
+	}
+	return ~(sum);
+}
+
+//funkcja wysy³aj¹ca jeden bajt do pamiêci o podanym adresie w odpowiednim dynamixelu
+//w trybie blokuj¹cym
+void sendData(uint8_t byte, uint8_t mem_addr, uint8_t ID, UART_HandleTypeDef *huart){
+	HAL_GPIO_WritePin(DIRECTION_PORT, DIRECTION_PIN, SEND);
+	uint8_t data[8];
+	data[0] = 0xFF;
+	data[1] = 0xFF;
+	data[2] = ID;
+	data[3] = 0x04;
+	data[4] = 0x03;
+	data[5] = mem_addr;
+	data[6] = byte;
+	data[7] = checkSum(data, 7);
+	HAL_UART_Transmit(huart, data, 8, 100);
+	HAL_GPIO_WritePin(DIRECTION_PORT, DIRECTION_PIN, RECEIVE);
+}
+
+//funkcja wysy³aj¹ca dwa bajty do pamiêci o podanym adresie w odpowiednim dynamixelu
+//w trybie blokuj¹cym
+void sendData2(uint8_t byteLow, uint8_t byteHigh, uint8_t mem_addr, uint8_t ID, UART_HandleTypeDef *huart){
+	HAL_GPIO_WritePin(DIRECTION_PORT, DIRECTION_PIN, SEND);
+	uint8_t data[9];
+	data[0] = 0xFF;
+	data[1] = 0xFF;
+	data[2] = ID;
+	data[3] = 0x05;
+	data[4] = 0x03;
+	data[5] = mem_addr;
+	data[6] = byteLow;
+	data[7] = byteHigh;
+	data[8] = checkSum(data, 8);
+	HAL_UART_Transmit(huart, data, 8, 100);
+	HAL_GPIO_WritePin(DIRECTION_PORT, DIRECTION_PIN, RECEIVE);
+}
+
+
+
+void ledOn(uint8_t ID, UART_HandleTypeDef *huart){
+	sendData(0x01, 0x18, ID, huart);
+}
+
+void ledOff(uint8_t ID, UART_HandleTypeDef *huart){
+	sendData(0x00, 0x18, ID, huart);
+}
+
+void setGoalPosition(uint16_t value, uint8_t ID, , UART_HandleTypeDef *huart){
+	byteLow = value & 0xFF;
+	value = value >> 8;
+	byteHigh = value & 0xFF;
+	sendData2(byteLow, byteHigh, 0x1E, ID, huart);
+}
+
+void setMovingSpeed(uint16_t value, uint8_t ID, , UART_HandleTypeDef *huart){
+	byteLow = value & 0xFF;
+	value = value >> 8;
+	byteHigh = value & 0xFF;
+	sendData2(byteLow, byteHigh, 0x20, ID, huart);
+}
+
+void setCWAngleLimit(uint16_t value, uint8_t ID, , UART_HandleTypeDef *huart){
+	byteLow = value & 0xFF;
+	value = value >> 8;
+	byteHigh = value & 0xFF;
+	sendData2(byteLow, byteHigh, 0x06, ID, huart);
+}
+
+void setCCWAngleLimit(uint16_t value, uint8_t ID, , UART_HandleTypeDef *huart){
+	byteLow = value & 0xFF;
+	value = value >> 8;
+	byteHigh = value & 0xFF;
+	sendData2(byteLow, byteHigh, 0x08, ID, huart);
+}
+
+//funckja zwracajaca zawartosc pamieci dostepnej pod podanym adresem w danym dynamixelu
+int receiveData(uint8_t *read_value, uint8_t mem_addr, uint8_t ID){
+	readValue[0] = 0x00;//inicjalizacja poczatkowego bajtu w celu sprawdzenia poprawnosci odczytu
+						//pewnie mozna na podstawie wartosci zwracanej przez funkcje hal_uart_receive
+						//ale trudno, dokumentacja jest za dluga :(
+	HAL_GPIO_WritePin(DIRECTION_PORT, DIRECTION_PIN, SEND);
+	uint8_t request[8];
+	request[0] = 0xFF;
+	request[1] = 0xFF;
+	request[2] = ID;
+	request[3] = 0x04;
+	request[4] = 0x02;
+	request[5] = mem_addr;
+	request[6] = 0x01;
+	request[7] = checkSum(request, 7);
+	HAL_UART_Transmit(huart, request, 8, 100);
+	HAL_GPIO_WritePin(DIRECTION_PORT, DIRECTION_PIN, RECEIVE);
+	HAL_UART_Receive(huart, read_value, 7);
+	if(read_value[0]==0xFF) return 0;
+	else return -1;
+}
+
+//funckja zwracajaca zawartosc dwoch bajtow pod podanym adresem w danym dynamixelu
+int receiveData2(uint8_t *read_value, uint8_t mem_addr, uint8_t ID){
+	readValue[0] = 0x00;
+	HAL_GPIO_WritePin(DIRECTION_PORT, DIRECTION_PIN, SEND);
+	uint8_t request[8];
+	request[0] = 0xFF;
+	request[1] = 0xFF;
+	request[2] = ID;
+	request[3] = 0x04;
+	request[4] = 0x02;
+	request[5] = mem_addr;
+	request[6] = 0x02;
+	request[7] = checkSum(request, 7);
+	HAL_UART_Transmit(huart, request, 8, 100);
+	HAL_GPIO_WritePin(DIRECTION_PORT, DIRECTION_PIN, RECEIVE);
+	HAL_UART_Receive(huart, read_value, 8);
+	if(read_value[0]==0xFF) return 0;
+	else return -1;
+}
---
+++ robocik/Src/main.c
@@ -0,0 +1,458 @@
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  ** This notice applies to any and all portions of this file
+  * that are not between comment pairs USER CODE BEGIN and
+  * USER CODE END. Other portions of this file, whether 
+  * inserted by the user or by software development tools
+  * are owned by their respective copyright owners.
+  *
+  * COPYRIGHT(c) 2018 STMicroelectronics
+  *
+  * Redistribution and use in source and binary forms, with or without modification,
+  * are permitted provided that the following conditions are met:
+  *   1. Redistributions of source code must retain the above copyright notice,
+  *      this list of conditions and the following disclaimer.
+  *   2. Redistributions in binary form must reproduce the above copyright notice,
+  *      this list of conditions and the following disclaimer in the documentation
+  *      and/or other materials provided with the distribution.
+  *   3. Neither the name of STMicroelectronics nor the names of its contributors
+  *      may be used to endorse or promote products derived from this software
+  *      without specific prior written permission.
+  *
+  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  *
+  ******************************************************************************
+  */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "stm32f3xx_hal.h"
+
+/* USER CODE BEGIN Includes */
+#include "defines.h"
+#include "tm_stm32_disco.h"
+#include "tm_stm32_delay.h"
+#include "tm_stm32_fatfs.h"
+#include "tm_stm32_lcd.h"
+#include "stdio.h"
+
+
+/* USER CODE END Includes */
+
+/* Private variables ---------------------------------------------------------*/
+SPI_HandleTypeDef hspi2;
+SPI_HandleTypeDef hspi3;
+
+UART_HandleTypeDef huart5;
+
+/* USER CODE BEGIN PV */
+/* Private variables ---------------------------------------------------------*/
+uint8_t check = 0;
+
+/* Fatfs structure */
+
+
+FATFS mynewdiskFatFs;  /* File system object for User logical drive */
+FIL MyFile;            /* File object */
+FRESULT fres;
+
+/* Size structure for FATFS */
+TM_FATFS_Size_t CardSize;
+
+/* Buffer variable */
+char buffer[100];
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_SPI2_Init(void);
+static void MX_UART5_Init(void);
+static void MX_SPI3_Init(void);
+
+/* USER CODE BEGIN PFP */
+/* Private function prototypes -----------------------------------------------*/
+
+uint8_t toSyncWrite(); //Przygotowuje tablicê danych, zawierajaca instrukcje SyncWrite
+void syncWrite (uint8_t *stan); //Przesy³a instrukcjê SyncWrite do Dynamixeli
+void turnonLED (); // W³¹cza diodê LED w pojedynczym Dynamixelu
+uint8_t readSPI();
+void move();
+
+/* USER CODE END PFP */
+
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  *
+  * @retval None
+  */
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration----------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_SPI2_Init();
+  MX_UART5_Init();
+  MX_SPI3_Init();
+  /* USER CODE BEGIN 2 */
+
+  if(FATFS_LinkDriver(&mynewdisk_Driver, mynewdiskPath) == 0)
+    {
+      if(f_mount(&mynewdiskFatFs, (TCHAR const*)mynewdiskPath, 0) == FR_OK)
+      {
+        if(f_open(&MyFile, "STM32.TXT", FA_CREATE_ALWAYS | FA_WRITE) == FR_OK)
+        {
+          if(f_write(&MyFile, wtext, sizeof(wtext), (void *)&wbytes) == FR_OK);
+          {
+            f_close(&MyFile);
+          }
+        }
+      }
+    }
+    FATFS_UnLinkDriver(mynewdiskPath);
+
+
+  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_15, GPIO_PIN_SET);
+  turnonLED();
+  //move();
+
+
+
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+	 // HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
+	 //    HAL_Delay(500);
+  /* USER CODE END WHILE */
+
+  /* USER CODE BEGIN 3 */
+
+  }
+  /* USER CODE END 3 */
+
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+
+  RCC_OscInitTypeDef RCC_OscInitStruct;
+  RCC_ClkInitTypeDef RCC_ClkInitStruct;
+  RCC_PeriphCLKInitTypeDef PeriphClkInit;
+
+    /**Initializes the CPU, AHB and APB busses clocks 
+    */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = 16;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL9;
+  RCC_OscInitStruct.PLL.PREDIV = RCC_PREDIV_DIV1;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    _Error_Handler(__FILE__, __LINE__);
+  }
+
+    /**Initializes the CPU, AHB and APB busses clocks 
+    */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+  {
+    _Error_Handler(__FILE__, __LINE__);
+  }
+
+  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_UART5;
+  PeriphClkInit.Uart5ClockSelection = RCC_UART5CLKSOURCE_PCLK1;
+  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
+  {
+    _Error_Handler(__FILE__, __LINE__);
+  }
+
+    /**Configure the Systick interrupt time 
+    */
+  HAL_SYSTICK_Config(HAL_RCC_GetHCLKFreq()/1000);
+
+    /**Configure the Systick 
+    */
+  HAL_SYSTICK_CLKSourceConfig(SYSTICK_CLKSOURCE_HCLK);
+
+  /* SysTick_IRQn interrupt configuration */
+  HAL_NVIC_SetPriority(SysTick_IRQn, 0, 0);
+}
+
+/* SPI2 init function */
+static void MX_SPI2_Init(void)
+{
+
+  /* SPI2 parameter configuration*/
+  hspi2.Instance = SPI2;
+  hspi2.Init.Mode = SPI_MODE_MASTER;
+  hspi2.Init.Direction = SPI_DIRECTION_2LINES;
+  hspi2.Init.DataSize = SPI_DATASIZE_4BIT;
+  hspi2.Init.CLKPolarity = SPI_POLARITY_LOW;
+  hspi2.Init.CLKPhase = SPI_PHASE_1EDGE;
+  hspi2.Init.NSS = SPI_NSS_SOFT;
+  hspi2.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_2;
+  hspi2.Init.FirstBit = SPI_FIRSTBIT_MSB;
+  hspi2.Init.TIMode = SPI_TIMODE_DISABLE;
+  hspi2.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
+  hspi2.Init.CRCPolynomial = 7;
+  hspi2.Init.CRCLength = SPI_CRC_LENGTH_DATASIZE;
+  hspi2.Init.NSSPMode = SPI_NSS_PULSE_ENABLE;
+  if (HAL_SPI_Init(&hspi2) != HAL_OK)
+  {
+    _Error_Handler(__FILE__, __LINE__);
+  }
+
+}
+
+/* SPI3 init function */
+static void MX_SPI3_Init(void)
+{
+
+  /* SPI3 parameter configuration*/
+  hspi3.Instance = SPI3;
+  hspi3.Init.Mode = SPI_MODE_MASTER;
+  hspi3.Init.Direction = SPI_DIRECTION_2LINES;
+  hspi3.Init.DataSize = SPI_DATASIZE_4BIT;
+  hspi3.Init.CLKPolarity = SPI_POLARITY_LOW;
+  hspi3.Init.CLKPhase = SPI_PHASE_1EDGE;
+  hspi3.Init.NSS = SPI_NSS_SOFT;
+  hspi3.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_2;
+  hspi3.Init.FirstBit = SPI_FIRSTBIT_MSB;
+  hspi3.Init.TIMode = SPI_TIMODE_DISABLE;
+  hspi3.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
+  hspi3.Init.CRCPolynomial = 7;
+  hspi3.Init.CRCLength = SPI_CRC_LENGTH_DATASIZE;
+  hspi3.Init.NSSPMode = SPI_NSS_PULSE_ENABLE;
+  if (HAL_SPI_Init(&hspi3) != HAL_OK)
+  {
+    _Error_Handler(__FILE__, __LINE__);
+  }
+
+}
+
+/* UART5 init function */
+static void MX_UART5_Init(void)
+{
+
+  huart5.Instance = UART5;
+  huart5.Init.BaudRate = 115200;
+  huart5.Init.WordLength = UART_WORDLENGTH_8B;
+  huart5.Init.StopBits = UART_STOPBITS_1;
+  huart5.Init.Parity = UART_PARITY_NONE;
+  huart5.Init.Mode = UART_MODE_TX_RX;
+  huart5.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart5.Init.OverSampling = UART_OVERSAMPLING_16;
+  huart5.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
+  huart5.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
+  if (HAL_UART_Init(&huart5) != HAL_OK)
+  {
+    _Error_Handler(__FILE__, __LINE__);
+  }
+
+}
+
+/** Configure pins as 
+        * Analog 
+        * Input 
+        * Output
+        * EVENT_OUT
+        * EXTI
+     PA2   ------> USART2_TX
+     PA3   ------> USART2_RX
+*/
+static void MX_GPIO_Init(void)
+{
+
+  GPIO_InitTypeDef GPIO_InitStruct;
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOF_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+  __HAL_RCC_GPIOD_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOA, LD2_Pin|GPIO_PIN_15, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin : B1_Pin */
+  GPIO_InitStruct.Pin = B1_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(B1_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : USART_TX_Pin USART_RX_Pin */
+  GPIO_InitStruct.Pin = USART_TX_Pin|USART_RX_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  GPIO_InitStruct.Alternate = GPIO_AF7_USART2;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : LD2_Pin PA15 */
+  GPIO_InitStruct.Pin = LD2_Pin|GPIO_PIN_15;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+}
+
+/* USER CODE BEGIN 4 */
+
+uint8_t toSyncWrite() //Kontroluje kilka Dynamixeli na raz. Ta sama instrukcja dla wszystkich, parametry instrukcji dla ka¿dego Dynamixela oddzielnie, po ID
+{
+	uint8_t sent[11]; //Tworzy tablicê, która bêdzie wysy³ana
+	sent[0] = 0xFF;
+	sent[1] = 0xFF;
+	sent[2] = 0xFE; //ID - dla SyncWrite jak dla broadcastu
+	sent[3] = (4+1) + 4; // D³ugoœc calej instrukcji
+	sent[4] = 0x83; //Wysy³ana instrukcja
+	sent[5] = 0x1E; //Adres pocz¹tkowy pamiêci
+	sent[6] = 0x04;//D³ugoœ
+	sent[7] = 0x00;//ID1
+	sent[8] = 0x10;
+	sent[9] = 0x00;
+	sent[10] = 0x50;
+	sent[11] = 0x01;
+	return &sent;
+}
+void turnonLED()
+{
+	check = 1;
+	uint8_t sent[9];
+		sent[0] = 0xFF;
+		sent[1] = 0xFF;
+		sent[2] = 0xFE; //ID
+		sent[3] = 0x05; // Length
+		sent[4] = 0x03; //Instruction?
+		sent[5] = 0x18;//Value1?->Adress
+		sent[6] = 0x01;//Value2?
+		sent[7] = 0x01;
+		sent[8] = ~(sent[2] + sent[3] + sent[4] + sent[5]+sent[6]+sent[7]); // checksum
+		HAL_UART_Transmit(&huart5,&sent,9,100);
+}
+void move()
+{
+	check = 1;
+	uint8_t sent[11];
+		sent[0] = 0xFF;
+		sent[1] = 0xFF;
+		sent[2] = 0x03; //ID
+		sent[3] = 0x07; // Length
+		sent[4] = 0x03;
+		sent[5] = 0x1E;
+		sent[6] = 0x00;
+		sent[7] = 0x02;
+		sent[8] = 0x00;
+		sent[9] = 0x02;
+		sent[10] = ~(sent[2] + sent[3] + sent[4] + sent[5]+sent[6]+sent[7] + sent[8]+sent[9]); //checksum
+		HAL_UART_Transmit(&huart5,&sent,11,100);
+}
+void syncWrite (uint8_t *stan)
+{
+	HAL_UART_Transmit(&huart5, &stan,8,100);
+}
+
+uint8_t readSPI()
+{
+
+	uint8_t data[12];
+	HAL_SPI_Receive(&hspi2, data, 12, 0xFF);
+	return &data;
+}
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @param  file: The file name as string.
+  * @param  line: The line in file as a number.
+  * @retval None
+  */
+void _Error_Handler(char *file, int line)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  while(1) 
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t* file, uint32_t line)
+{ 
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+    ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
+
+/**
+  * @}
+  */
+
+/**
+  * @}
+  */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
